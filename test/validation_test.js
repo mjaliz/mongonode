a525b91d0b95acc6a296533e223901a3e53bc6aa
MohammadJavad14 <mrph14@gmail.com>
2023-09-05 22:00:57 +0330

    requiring attributes on a model

---
+++ test/validation_test.js
@@ -0,0 +1,12 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Validating records", () => {
+  it("requires a user name", () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name.properties;
+
+    assert(message === "Name is required.");
+  });
+});
